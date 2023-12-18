c68af1e8031a97a6369a278266dba04602a14687
chabuuuu <haphuthinh332004@gmail.com>
2023-12-18 16:03:34 +0700

    feauture/tele-send-backup

--- src/services/backup/index.ts
+++ src/services/backup/index.ts
@@ -5,7 +5,14 @@ const compress = require('gzipme');
 const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const TelegramBot = require('node-telegram-bot-api');
 
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELE_BOT_TOKEN;
+const chat_id = process.env.TELE_CHAT_ID;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: false});
 // writing postgresql backup function
 export async function takePGBackup() {
     // getting db connection parameters from environment file
@@ -14,15 +21,16 @@ const database = "WorkerService";
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
 const dbPass = process.env.DB_PASSWORD;
-
+const backupDir = process.env.BACKUP_DB
 // defining backup file name
 const date = new Date();
-const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getTime()}`;
 const backupFile= `loan_system-${today}.tar`;
 
-    await exec(`PGPASSWORD=${dbPass} pg_dump -U ${username} -F t LOAN_SYSTEM > /home/haphuthinh/${backupFile}`)
+    await exec(`PGPASSWORD=${dbPass} pg_dump -U ${username} -F t LOAN_SYSTEM > ${backupDir}/${backupFile}`)
     console.log('Done create backup');
-    
+    bot.sendMessage(chat_id, "Your database backup file: ");
+    bot.sendDocument(chat_id, `${backupDir}/${backupFile}` )
 }
 
 // calling postgresql backup function
--- src/services/message/receive.service.ts
+++ src/services/message/receive.service.ts
@@ -1,5 +1,4 @@
 import { takePGBackup } from "../backup";
-
 const amqplib = require('amqplib');
 const amqp_url_cloud = 'amqps://oxrmbads:huO93XWEqbo-NdbugJUqtt3T_f_RahYs@octopus.rmq3.cloudamqp.com/oxrmbads'
 const amqp_url_docker = 'amqp://localhost:5672'
